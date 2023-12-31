08a7612afacef5711d36636db37c43502757cf25
adithyaanup2002@gmail.com <adithyaanup2002@gmail.com>
2023-12-31 23:19:15 +0530

    Added code for Servo-reverse

--- src/main.cpp
+++ src/main.cpp
@@ -396,8 +396,9 @@ void servoMotor() {
     delay(100);
     handleEchoSensor(pos);
   }
-  // for(pos = 180; pos >= 0; pos -= 1){
-  //   servo.write(pos);
-  //   delay(15);
-  // }
+  for(pos = 180; pos >= 0; pos -= 10){
+    servo.write(pos);
+    delay(100);
+    handleEchoSensor(pos);
+  }
 }
