536d287e6e08f6944602cb31b5f705b768d65a2b
adithyaanup2002@gmail.com <adithyaanup2002@gmail.com>
2024-01-26 23:25:48 +0530

    Configured Alexa Server

--- Alexa_Server/index.js
+++ Alexa_Server/index.js
@@ -7,6 +7,32 @@ dotenv.config();
 const port = process.env.PORT;
 const app = express();
 
+// end point defined to handle requests coming from alexa. "move" is the name of the endpoint
+
+const alexaApp = new alexa.app("move")
+
+// endpoint is linked to express router
+
+alexaApp.express({
+    expressApp: app,
+
+    checkCert: false,
+
+    debug: true
+})
+
+alexaApp.launch((req,res) => {
+    res.say("App is launched!");
+})
+
+// intent as well as utterances defined
+
+alexaApp.intent('Start', {
+    "utterances": ["Start the rover"]
+},(req,res) => {
+    res.say("Rover has started")
+})
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 })
